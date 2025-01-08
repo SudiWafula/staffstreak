72218f5f14f03ddfd3806c423a68c991314dd763
SudiWafula <sudipapa1994@gmail,com>
2025-01-08 19:18:25 +0000

    Initial commit

---
+++ staffstreak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+st.title("Welcome to StaffStreak App!")
+st.write("This is my StaffStreak app using Streamlit.")
+
+
+# In[2]:
+
+
+import os
+os.getcwd()
+
+
+# In[ ]:
+
+
+
+
